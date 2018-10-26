fe4a8dd650c9155b8c12b39c84ba87f0718ace1a
Arhuinternacional <info@arhuinternacional.com>
2018-10-26 09:37:35 -0500

    usuario controller v1.1

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use Auth;
-use Yajra\Datatables\Datatables;
 
 class UsuarioController extends Controller
 {
@@ -37,19 +36,24 @@ class UsuarioController extends Controller
      */
     public function store(Request $request)
     {
-       // dd($request->all());
-            User::create([
-            'dni' => $request['dni'],
-            'name' => $request['name'],
-            'email' => $request['email'],
-            'password' => bcrypt($request['password']),
-            'username' => $request['username'],
-            'telefono' => $request['telefono'],
-            'perfil' => $request['perfil'],
+        $validator = \Validator::make($request->all(), [
+            'username' => 'required|unique:mongodb.users,username|min:4',
+            'password' => 'required|min:4',
+            'email' => 'required|email|min:4'
+        ]);
 
-            ]);
+        if ($validator->fails()) {
+            return response()->json('Datos incompletos');
+        }
 
-     }
+        $user = new User;
+        $user->username = $request->username;
+        $user->password = bcrypt($request->password);
+        $user->email = $request->email;
+        $user->save();
+
+        return response()->json(true);
+    }
 
     /**
      * Display the specified resource.
@@ -59,7 +63,7 @@ class UsuarioController extends Controller
      */
     public function show($id)
     {
-        return response()->json('hola');
+        //return response()->json();
     }
 
     /**
@@ -82,7 +86,29 @@ class UsuarioController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //return response()->json($request->all());
+
+        $validator = \Validator::make($request->all(), [
+            'username' => 'min:4',
+            'email' => 'email|min:4',
+            'password' => 'min:4|max:16'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json('Datos incompletos');
+        }
+
+        $user = User::where('username', $id)->first();
+
+        if ($user != null) {
+            $user->email = $request->email != $user->email ? $request->email : $user->email;
+            $user->username = $request->username != $user->username ? $request->username : $user->username;
+            $user->password = $request->password != $user->password && $request->password != '' ? bcrypt($request->password) : $user->password;
+            $user->save();
+        }
+
+        return response()->json(true);
+
     }
 
     /**
@@ -96,11 +122,33 @@ class UsuarioController extends Controller
         //
     }
 
-    public function GetUser()
+    public function GetUser(Request $request)
     {
-        //return response()->json("holasss");
-       $user = User::getFresh();
-       
+        if ($request->filter != null) {
+            $user = User::where('email', 'like', '%'.$request->filter.'%')
+                        ->orWhere('username', 'like', '%'.$request->filter.'%')
+                        ;
+        }else{
+            $user = User::getFresh();
+        }
+
+        switch ($request->sort) {
+          case 'email|asc':
+            $user->orderBy('email', 'asc');
+            break;
+          case 'email|desc':
+            $user->orderBy('email', 'desc');
+            break;
+          case 'username|asc':
+            $user->orderBy('username', 'asc');
+            break;
+          case 'username|desc':
+            $user->orderBy('username', 'desc');
+            break;
+          default:
+            $user->orderBy('_id', 'desc');
+            break;
+        }
        return response()->json($user->paginate(10));
     }
 }
